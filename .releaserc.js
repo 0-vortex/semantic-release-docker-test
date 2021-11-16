069ece2798ec7d8f802cf36a37a03cfaa02123dd
TED Vortex <ted.vortex@gmail.com>
2021-11-17 01:18:50 +0200

    test: use external action

---
+++ .releaserc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branch: 'main',
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+};
