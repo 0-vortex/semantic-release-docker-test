7f76c76dcba1af6369f8c00c8d4712b0a391bd72
TED Vortex <ted.vortex@gmail.com>
2021-11-17 01:28:26 +0200

    fix: add proper release branch configuration

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,7 @@
 module.exports = {
-    branch: 'main',
+    branches: [
+        'main'
+    ],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
