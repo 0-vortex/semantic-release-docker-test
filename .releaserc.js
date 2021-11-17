800d05fc47920ee34bb3550e6c2ade8a4e1ed487
TED Vortex <ted.vortex@gmail.com>
2021-11-17 02:07:19 +0200

    test: add more compatible approach

--- .releaserc.js
+++ .releaserc.js
@@ -5,6 +5,24 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
+        "@semantic-release/changelog",
+        ["@semantic-release/git", {
+            "assets": [
+                "CHANGELOG.md",
+                "package.json",
+                "package-lock.json",
+                "npm-shrinkwrap.json",
+                "public/diagram.svg"
+            ],
+            "message": `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`
+        }],
+        ["@semantic-release/github", {
+            "assets": [
+                {
+                    "path": "pack/*.tgz",
+                    "label": "Static distribution"
+                }
+            ]
+        }],
     ]
 };
