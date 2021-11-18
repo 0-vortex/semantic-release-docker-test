6e5f38c1a96a3d182363416a49d97a0f69031075
TED Vortex <ted.vortex@gmail.com>
2021-11-18 02:15:48 +0200

    feat: use open sauced semantic configuration without installing

--- .releaserc.js
+++ .releaserc.js
@@ -1,28 +1,3 @@
 module.exports = {
-    branches: [
-        'main'
-    ],
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        ["@semantic-release/git", {
-            "assets": [
-                "CHANGELOG.md",
-                "package.json",
-                "package-lock.json",
-                "npm-shrinkwrap.json",
-                "public/diagram.svg"
-            ],
-            "message": `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`
-        }],
-        ["@semantic-release/github", {
-            "assets": [
-                {
-                    "path": "pack/*.tgz",
-                    "label": "Static distribution"
-                }
-            ]
-        }],
-    ]
+    extends: 'open-sauced-semantic-config-test'
 };
